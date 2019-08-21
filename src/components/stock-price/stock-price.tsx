655b96d1c45850bf92640cc8818ba8566ea01375
Daniel Segura <daniel@MacBook-Pro-de-Daniel.local>
2019-08-21 15:24:10 -0500

    HTTP Requests in Components

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core'
+import { Component, State, h } from '@stencil/core'
 
 @Component({
   tag: 'uc-stock-price',
@@ -7,9 +7,33 @@ import { Component, h } from '@stencil/core'
 })
 
 export class StockPrice {
+  @State() fetchedPrice: number
+
+  onFetchStockPrice = (e: Event) => {
+    e.preventDefault();
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo`)
+    .then((res) => {
+      return res.json();
+    })
+    .then(parsedRes => {
+      console.log(parsedRes);
+      this.fetchedPrice = +parsedRes['Global Quote']['05. price']
+      console.log(this.fetchedPrice);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
-    return (
-      <p>Stock Price</p>
-    )
+    return [
+      <form onSubmit={this.onFetchStockPrice}>
+        <input id="stock-symbol" type="text"/>
+        <button type="submit">Fetch Data</button>
+      </form>,
+      <div>
+        <p>Price: ${this.fetchedPrice} </p>
+      </div>
+    ]
   }
 }
